70a3cb785e744aa630253d1829341011c1c2a71a
Farah Desktop <farahkrecinic@gmail.com>
2021-02-07 15:10:12 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ class App extends Component{
       monsters: [],
       searchField: ''
     };
+
   }
 
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
+  }
+
+  /* Javascript by default doesn't set its scope of 'this' so to have it recognized
+  that's why we create the function as equal to an arrow function (e) =>  */
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
   }
 
   render() {
@@ -29,9 +36,10 @@ class App extends Component{
     )
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
-          handleChange = {e => this.setState({searchField: e.target.value})}
+          handleChange = {this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList> 
       </div>
